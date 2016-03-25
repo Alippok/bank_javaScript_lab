e54f01b69fb263a53cdb592aa883f29a3229db1f
A_Lippok <alippok@hotmail.co.uk>
2016-03-25 09:05:27 +0000

    Adds bank constructor function and checks first test

---
+++ bank.js
@@ -0,0 +1,11 @@
+
+
+
+var Bank = function(name){
+  this.name = name;
+  this.accountsHolder = [];
+};
+
+
+
+module.exports = Bank;
---
+++ specs/bank_specs.js
@@ -0,0 +1,21 @@
+var assert = require('chai').assert;
+var Bank = require('../bank.js');
+var Account = require('../account.js')
+
+var bank1 = new Bank("Gringotts");
+var account1 = new Account("Draco Malfoy", 7500, 'wizard');
+var account2 = new Account("Hagrid", 2300, 'ministerial');
+var account3 = new Account("Hagrid", 140, 'muggle')
+var account4 = new Account("Harry Potter", 250, 'muggle')
+var account5 = new Account("Hermoine Grainger", 500, 'muggle')
+var account6 = new Account("Mr Weasley", 3000, 'ministerial')
+
+describe("Bank", function(){
+  
+  it("should have a name", function(){
+    assert.equal("Gringotts", bank1.name)
+  })
+
+
+
+});
